6f888e696028ed2e651bae091cafc6c8c34c6766
murikbmx <murikbmx@gmail.com>
2026-03-09 16:54:38 +0500

    Remove Aperture & Twelve

--- config/common_mobile_full.mk
+++ config/common_mobile_full.mk
@@ -15,16 +15,6 @@ PRODUCT_PACKAGES += \
     Seedvault \
     Twelve
 
-ifneq ($(PRODUCT_NO_CAMERA),true)
-PRODUCT_PACKAGES += \
-    Aperture
-endif
-
-ifneq ($(TARGET_EXCLUDES_AUDIOFX),true)
-PRODUCT_PACKAGES += \
-    AudioFX
-endif
-
 # Extra cmdline tools
 PRODUCT_PACKAGES += \
     unrar \
