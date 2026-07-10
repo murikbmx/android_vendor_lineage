2e6de6c6acbbe0245516869daa0c0e11d30d4862
murikbmx <murikbmx@gmail.com>
2026-07-10 10:38:41 +0500

    Bring back Aperture
    Conditionally include 'Aperture' in PRODUCT_PACKAGES if camera is not disabled.

--- config/common_mobile_full.mk
+++ config/common_mobile_full.mk
@@ -15,6 +15,11 @@ PRODUCT_PACKAGES += \
     Seedvault \
     Twelve
 
+ifneq ($(PRODUCT_NO_CAMERA),true)
+PRODUCT_PACKAGES += \
+    Aperture
+endif
+
 # Extra cmdline tools
 PRODUCT_PACKAGES += \
     unrar \
